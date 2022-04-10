649dcde0e4f51dba378eca938f5af8bfcd058d7b
IotaHydraewq <writeforever@foxmail.com>
2022-04-10 17:38:52 +0800

    add a script to get toolchain
    Signed-off-by: IotaHydraewq <writeforever@foxmail.com>

---
+++ software/bsp/toolchain/get_toolchain.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+function wget_remote()
+{
+	echo "Downloading  $1"
+	wget $1
+	if [ $? -eq 0 ]; then
+		return 0
+	else
+		return 1
+	fi
+}
+
+function do_install_toolchain()
+{
+	echo "Installing toolchain to $1"
+	if [ -d $1 ]; then
+		tar xJvf gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabihf.tar.xz -C $1
+	else
+		exit -1
+	fi
+}
+
+wget_remote https://releases.linaro.org/components/toolchain/binaries/7.5-2019.12/arm-linux-gnueabihf/gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabihf.tar.xz
+
+# echo $?
+
+if [ "$?" -eq "0" ]; then
+	do_install_toolchain "/opt"
+fi
