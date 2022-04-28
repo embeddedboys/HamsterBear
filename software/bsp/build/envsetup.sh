22b9da13438e3fd1cb039d48c293fff51023f378
IotaHydrae <writeforever@foxmail.com>
2022-04-29 04:20:55 +0800

    update build scripts
    Signed-off-by: IotaHydrae <writeforever@foxmail.com>

--- software/bsp/build/envsetup.sh
+++ software/bsp/build/envsetup.sh
@@ -1,3 +1,8 @@
+# exporting kernel build target
 export ARCH=arm
 export CROSS_COMPILE=arm-linux-gnueabi-
 export PATH=$PATH:/opt/gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabi/bin/
+
+# export dirs
+export ROOT_DIR=/home/$(whoami)/source/HamsterBear/software/bsp/
+echo $ROOT_DIR
